8e478dce001879b2e5648e3f38df693c87cbf50d
yotam <yotam@trellis.ag>
2019-03-04 01:30:10 +0200

    adjust docker configuration such that localhost starts up

--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-
-cd /code
+cd /app
 echo "waiting on postgres"
-while ! psql -U postgres -c "SELECT 'DBD::Pg ping test'" 2>/dev/null; do
+while ! psql -U postgres -h db -c "SELECT 'DBD::Pg ping test'" 2>/dev/null; do
     sleep 3
     echo "..."
 done
-
-python3 manage.py migrate --noinput
-python3 manage.py collectstatic --noinput
-python3 manage.py runserver_plus 0.0.0.0:8000
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+python manage.py runserver 0.0.0.0:8000
